68422a2e07c7238569bc1808ed770c7e185514d9
mu5tafacool <mustafakulf1@gmail.com>
2022-04-20 15:20:38 +0300

    mustafa

--- src/soru1/Yurtdisi.java
+++ src/soru1/Yurtdisi.java
@@ -1,4 +1,46 @@
 package soru1;
 
-public class Yurtdisi {
+public class Yurtdisi extends Malzeme {
+
+    private String plaka;
+    private boolean Cekici;
+    private boolean dorse;
+
+    public String getPlaka() {
+        return plaka;
+    }
+
+    public void setPlaka(String plaka) {
+        this.plaka = plaka;
+    }
+
+    public boolean isCekici() {
+        return Cekici;
+    }
+
+    public void setCekici(boolean cekici) {
+        Cekici = cekici;
+    }
+
+    public boolean isDorse() {
+        return dorse;
+    }
+
+    public void setDorse(boolean dorse) {
+        this.dorse = dorse;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Yurtdisi{" +
+                "plaka='" + plaka + '\'' +
+                ", Cekici=" + Cekici +
+                ", dorse=" + dorse +
+                '}';
+    }
+
+    public Yurtdisi() {
+    }
 }
