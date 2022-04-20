4dbbf68ad4474343d3c7c099145cbb0bf3700476
mu5tafacool <mustafakulf1@gmail.com>
2022-04-20 14:26:18 +0300

    soru

--- src/soru1/S.java
+++ src/soru1/S.java
@@ -1,4 +1,9 @@
 package soru1;
 
 public class S {
+
+    /*
+    * Bir gumrukte gelen tirlarin yurt disi ve icine geldigi kontrolune gore class ayir
+    * getirdigi urunun icerini yazan method ve classlar yapin
+    * */
 }
